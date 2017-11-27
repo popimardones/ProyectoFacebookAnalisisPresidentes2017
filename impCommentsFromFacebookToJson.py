451d4a4a67c7bba332a152ac02704fadf9c9e4fc
popimardones <30806447+popimardones@users.noreply.github.com>
2017-11-27 14:56:46 -0300

    add comments
    x

--- impCommentsFromFacebookToJson.py
+++ impCommentsFromFacebookToJson.py
@@ -2,15 +2,22 @@ import requests
 import json
 
 #LO SIGUIENTE CREA UNA LISTA DEL CONTENIDO QUE TIENE UN ARCHIVO TXT
+#CAMBIAR POR CADA CANDIDATO
 fileRead = open('AlejandroNavarroPostIds2.txt')
+
 #crear una lista de todos los post ids, pero todos los items tienen \n al final
 presidente_txtList= fileRead.readlines()
+
 #saca el \n de todos los item de la lista
 presidenteSacarSlashNList= [i.replace('\n','') for i in presidente_txtList]
+
 #saca el user_id_ de todos los item de la lista porque queremos una lista de solo los postIds
+#CAMBIAR POR CADA CANDIDATO
 presidenteSoloPostId_List = [i.replace('10152723078_','') for i in presidenteSacarSlashNList]
-#sacar el ultimo termino de la lista porque por alguna razon es un item empty
+
+#sacar el ultimo termino de la lista porque el archivo tiene una linea en blanco
 presidenteSoloPostId_List = presidenteSoloPostId_List[:-1]
+
 #se puede ver esto al imprimir cada lista
 print(presidente_txtList)
 print(presidenteSacarSlashNList)
@@ -20,9 +27,10 @@ fileRead.close()
 #####################################
 
 graph_api_version = 'v2.9'
+#cambiar token cada hora aprox
 access_token = 'EAACEdEose0cBAOo0g2aGHWHQ6ILZBjZC5T4rtdX4SukdjiwDtEXRzc2wxW6rfZB12RjKznNGlxRbkBMyd26ZCyIxnxthZAAySfjtpdQrZC6Dd4Gj9V36vuA12KoqZCmAo33eSFq5Y3ZCNt1R54M0ZCsdRJdeOZA0Q2aAOtYWZCKSh1E9J4riQDx9MmMeyeBLBuFoZCkZD'
 
-#user id (id del presidente; CAMBIAR POR CADA PRESIDENTE)
+#user id (id del candidato; CAMBIAR POR CADA CANDIDATO)
 user_id = '10152723078'
 
 for id in presidenteSoloPostId_List:
@@ -50,6 +58,6 @@ for id in presidenteSoloPostId_List:
             print('Encontrados {} comments'.format(len(data['data'])))
         else:
             break
-
+#guardar los comentarios en un archivo json para cada publicación del candidato
 with open('AlejandroNavarroCommentsOfPost' + id + '.json', 'w', encoding='utf-8') as outfile:
     json.dump(comments, outfile, indent=4, ensure_ascii=False)
